c7662d5885f4210c2ef03d32fb687592dc1c6bb1
Julio Silva <juliob91@gmail.com>
2012-06-24 01:26:38 -0300

    adicionei opcao same-as e comentarios uteis

--- home/julio/Scripts/monitores.sh
+++ home/julio/Scripts/monitores.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-EXTERNAL_OUTPUT="HDMI1"
 INTERNAL_OUTPUT="LVDS1"
-# EXTERNAL_LOCATION may be one of: left, right, above, or below
+EXTERNAL_OUTPUT="HDMI1"
+#EXTERNAL_OUTPUT="VGA1"
+
+# EXTERNAL_LOCATION: left, right, above, below, same-as
 EXTERNAL_LOCATION="right"
  
 case "$EXTERNAL_LOCATION" in
@@ -19,11 +21,11 @@ case "$EXTERNAL_LOCATION" in
                EXTERNAL_LOCATION="--below $INTERNAL_OUTPUT"
                ;;
        *)
-               EXTERNAL_LOCATION="--left-of $INTERNAL_OUTPUT"
+               EXTERNAL_LOCATION="--same-as $INTERNAL_OUTPUT"
                ;;
 esac
  
-xrandr |grep $EXTERNAL_OUTPUT | grep " connected "
+xrandr | grep $EXTERNAL_OUTPUT | grep " connected "
 if [ $? -eq 0 ]; then
     xrandr --output $INTERNAL_OUTPUT --off --output $EXTERNAL_OUTPUT --auto $EXTERNAL_LOCATION
 else
@@ -31,5 +33,10 @@ else
 fi
 
 echo "sucesso!"
-
 #END
+
+# Tela do notebook com versão miniatura da TV (1920x1080)
+#xrandr --fb 1920x1080 --output LVDS1 --scale 1.405x1.406 --output HDMI1 --auto
+
+# TV com versão ampliada do notebook (1366x768)
+#xrandr --fb 1366x768 --output LVDS1 --auto --output HDMI1 --auto --scale 0.711x0.711
