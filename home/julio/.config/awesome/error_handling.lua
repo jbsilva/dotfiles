729248b9ec35e4d56b8ad76df6469a2e299fe4e2
Julio Silva <julio@juliobs.com>
2013-01-07 00:21:12 -0200

    Atualizacao para Awesome 3.5. Ainda faltam ajustes
    Mais detalhes: http://awesome.naquadah.org/wiki/Awesome_3.4_to_3.5

--- home/julio/.config/awesome/error_handling.lua
+++ home/julio/.config/awesome/error_handling.lua
@@ -1,3 +1,4 @@
+-- Copia exata do rc.lua original. Linhas 14 ate 37
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -10,7 +11,7 @@ end
 -- Handle runtime errors after startup
 do
     local in_error = false
-    awesome.add_signal("debug::error", function (err)
+    awesome.connect_signal("debug::error", function (err)
         -- Make sure we don't go into an endless error loop
         if in_error then return end
         in_error = true
