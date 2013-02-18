b278ee01b95843574fe37d0fc95ff7133fc46d67
Julio Silva <julio@juliobs.com>
2013-02-17 22:01:04 -0300

    Passa nome da rede por parametro

--- home/julio/Scripts/reconecta.sh
+++ home/julio/Scripts/reconecta.sh
@@ -3,7 +3,7 @@
 host1=google.com
 host2=wikipedia.org
 curr_date=`date +"%Y_%m_%d-%H:%M"`
-REDE=eth0-static-sanca
+REDE=$2
 
 UP=`((ping -w5 -c3 $host1 || ping -w5 -c3 $host2) > /dev/null 2>&1) && echo 1 || echo 0`
 
