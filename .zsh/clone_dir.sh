7f530dcea893d4d3c8a3cbc17b93aa34c43164b0
Julio Batista Silva <julio@juliobs.com>
2015-10-21 19:22:27 -0200

    Usage, allows 1 or 2 args, better parameters

--- .zsh/clone_dir.sh
+++ .zsh/clone_dir.sh
@@ -2,13 +2,43 @@
 function clone_dir()
 {
 
+    local usage="$(
+    cat <<EOF
+Uso:
+Clona a pasta ORIGEM em DESTINO:  $0 ORIGEM DESTINO
+Clona diretório atual em DESTINO: $0 DESTINO
+
+EOF
+    )"
+
     if (( ! $+commands[rsync] )); then
         return 1
     fi
 
-    cleand $DIR
 
-    local ORIGEM="${1:-'.'}"
-    local DESTINO="${2}"
-    rsync -aruv --delete "$ORIGEM" "$DESTINO"
+    if (( $# == 1 )); then
+        local ORIGEM="$PWD"
+        local DESTINO="${1}"
+    elif (( $# == 2 )); then
+        local ORIGEM="${1}"
+        local DESTINO="${2}"
+    else
+      print "$usage" >&2
+      return 1
+    fi
+
+    cleand $ORIGEM
+
+    rsync --progress \
+        --verbose \
+        --recursive \
+        --links \
+        --perms \
+        --owner \
+        --group \
+        --times \
+        --devices \
+        --specials \
+        --update \
+        --delete "$ORIGEM" "$DESTINO"
 }
