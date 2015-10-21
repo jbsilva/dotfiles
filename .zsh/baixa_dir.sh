362fd8b42b1bf8c4aa78922848b1eeb416fe7cd5
Julio Batista Silva <julio@juliobs.com>
2015-10-21 19:23:28 -0200

    Better parameters, indentation, example

--- .zsh/baixa_dir.sh
+++ .zsh/baixa_dir.sh
@@ -1,5 +1,6 @@
 # Downloads directories recursively. Mirror remote.
-# `baixa_dir $cut_dirs $level $url`
+# `baixa_dir $url $cut_dirs $level`
+# Example with login: http://my_username:my_password@files.juliobs.com/Images/
 function baixa_dir()
 {
     if (( ! $+commands[wget] )); then
@@ -11,14 +12,18 @@ function baixa_dir()
     local UserAgent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
 
     wget -e robots=off \
-    --user-agent="${UserAgent}" \
-    --cut-dirs=${CUT_DIRS} \
-    --no-parent \
-    --recursive \
-    --relative \
-    --level=${LEVEL} \
-    --no-host-directories \
-    --no-check-certificate \
-    --reject="index.html*" \
-    "$1"
+        --continue \
+        --timestamping \
+        --user-agent="${UserAgent}" \
+        --cut-dirs=${CUT_DIRS} \
+        --no-parent \
+        --recursive \
+        --relative \
+        --level=${LEVEL} \
+        --no-host-directories \
+        --no-check-certificate \
+        --reject="index.html*" \
+        --append-output=wget_log.txt \
+        --verbose \
+        "$1"
 }
