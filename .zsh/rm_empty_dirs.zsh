17270487ce48a08977b38d17afe3a0a5f712c9d8
Julio Batista Silva <julio@juliobs.com>
2016-07-10 21:41:44 -0300

    Replace dot with PWD

--- .zsh/rm_empty_dirs.zsh
+++ .zsh/rm_empty_dirs.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
-# Remove empty directories.
+# Remove empty directories. Works better after running `cleand`.
 # Requires GNU find
 function rm_empty_dirs()
 {
-    local DIR="${1:-'.'}"
+    local DIR="${1:-$PWD}"
     find ${DIR} -type d -empty -delete
     #find ${DIR} -depth -type d -empty -print0 | xargs -0 rmdir
 }
