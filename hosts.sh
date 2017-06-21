4e4af75759988ff333bfd16538b81851729acaee
Julio Batista Silva <julio@juliobs.com>
2017-06-21 20:23:31 -0300

    Block Youtube ads

--- hosts.sh
+++ hosts.sh
@@ -4,6 +4,23 @@ curl --output /etc/hosts --url "http://someonewhocares.org/hosts/zero/hosts"
 
 echo "
 ###############################################################################
+#<Youtube>
+0.0.0.0	i1.ytimg.com
+0.0.0.0	i9.ytimg.com
+0.0.0.0	r1---sn-vgqsen7z.googlevideo.com
+0.0.0.0	r1.sn-vgqsen7z.googlevideo.com
+0.0.0.0	r17---sn-vgqsenes.googlevideo.com
+0.0.0.0	r18---sn-4g57knd7.googlevideo.com
+0.0.0.0	r2---sn-4g5e6n7d.googlevideo.com
+0.0.0.0	r2---sn-vgqs7n7k.googlevideo.com
+0.0.0.0	r20---sn-vgqs7ne7.googlevideo.com
+0.0.0.0	r20.sn-vgqs7ne7.googlevideo.com
+0.0.0.0	r3---sn-4g57kn7e.googlevideo.com
+0.0.0.0	r4---sn-vgqs7nez.googlevideo.com
+0.0.0.0	r4.sn-vgqs7nez.googlevideo.com
+0.0.0.0	r5---sn-5hne6n7s.googlevideo.com
+0.0.0.0	www.youtube-nocookie.com
+###############################################################################
 #<Adobe>
 0.0.0.0	3dns-2.adobe.com
 0.0.0.0	3dns-3.adobe.com
