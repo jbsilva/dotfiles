ceb341a842412e64a243ce78dc4c6d5a9ca345e2
Julio Batista Silva <julio@juliobs.com>
2019-02-24 14:35:11 -0300

    Mais parâmetros no clone_dir

--- .zsh/clone_dir.zsh
+++ .zsh/clone_dir.zsh
@@ -43,11 +43,12 @@ EOF
     args+=(--devices)           # Transfer character and block device files
     args+=(--specials)          # Transfer special files such as named sockets and fifos
     args+=(--delete)            # Delete files not SRC
-    args+=(--whole-file)        # Don't use delta-xfer algorithm
+    #args+=(--whole-file)        # Don't use delta-xfer algorithm
     args+=(--compress-level=0)  # No compression
     args+=(--no-inc-recursive)  # Disable incremental recursion for better estimate (uses more memory)
     args+=(--info=progress2)    # Show total progress
     args+=(--human-readable)    # Output numbers in a more human-readable format
+    #args+=(--ignore-existing)   # Ignore existing
     #args+=(--size-only)         # Don't compare change date
     #args+=(--preallocate)       # Allocate dest files before writing
     #args+=(--progress)          # Show progress during transfer
