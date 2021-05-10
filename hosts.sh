da544633c6dc2268bf54d0d7f8d5fef484c27873
Julio Batista Silva <julio@juliobs.com>
2021-05-10 14:59:58 -0300

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -4,70 +4,7 @@ curl --output /etc/hosts --url "https://someonewhocares.org/hosts/zero/hosts"
 
 echo "
 ###############################################################################
-#<Youtube>
-0.0.0.0	i1.ytimg.com
-0.0.0.0	i9.ytimg.com
-0.0.0.0	r1---sn-vgqsen7z.googlevideo.com
-0.0.0.0	r1.sn-vgqsen7z.googlevideo.com
-0.0.0.0	r17---sn-vgqsenes.googlevideo.com
-0.0.0.0	r18---sn-4g57knd7.googlevideo.com
-0.0.0.0	r2---sn-4g5e6n7d.googlevideo.com
-0.0.0.0	r2---sn-vgqs7n7k.googlevideo.com
-0.0.0.0	r20---sn-vgqs7ne7.googlevideo.com
-0.0.0.0	r20.sn-vgqs7ne7.googlevideo.com
-0.0.0.0	r3---sn-4g57kn7e.googlevideo.com
-0.0.0.0	r4---sn-vgqs7nez.googlevideo.com
-0.0.0.0	r4.sn-vgqs7nez.googlevideo.com
-0.0.0.0	r5---sn-5hne6n7s.googlevideo.com
-0.0.0.0	www.youtube-nocookie.com
-###############################################################################
-#<Adobe>
-0.0.0.0	3dns-2.adobe.com
-0.0.0.0	3dns-3.adobe.com
-0.0.0.0	activate-sea.adobe.com
-0.0.0.0	activate-sjc0.adobe.com
-0.0.0.0	activate.adobe.com
-0.0.0.0	activate.wip3.adobe.com
-0.0.0.0	adobe-dns-2.adobe.com
-0.0.0.0	adobe-dns-3.adobe.com
-0.0.0.0	adobe-dns.adobe.com
-0.0.0.0	ereg.adobe.com
-0.0.0.0	ereg.wip3.adobe.com
-0.0.0.0	hl2rcv.adobe.com
-0.0.0.0	ims-na1-prprod.adobelogin.com
-0.0.0.0	lm.licenses.adobe.com
-0.0.0.0	na1r.services.adobe.com
-0.0.0.0	na2m-pr.licenses.adobe.com
-0.0.0.0	na4r.services.adobe.com
-0.0.0.0	practivate.adobe.com
-0.0.0.0	wip3.adobe.com
-0.0.0.0	wwis-dubc1-vip60.adobe.com
-0.0.0.0 lmlicenses.wip4.adobe.com
-0.0.0.0 lm.licenses.adobe.com
-0.0.0.0 hlrcv.stage.adobe.com
-#</Adobe>
-###############################################################################
-#<Spotify>
-0.0.0.0 pubads.g.doubleclick.net
-0.0.0.0 securepubads.g.doubleclick.net
-0.0.0.0 www.googletagservices.com
-0.0.0.0 gads.pubmatic.com
-0.0.0.0 ads.pubmatic.com
-#</Spotify>
-###############################################################################
-#<VueScan>
-0.0.0.0 www.hamrick.com
-0.0.0.0 static.hamrick.com
-0.0.0.0 stats.hamrick.com
-0.0.0.0 104.131.17.148 
-#</VueScan>
-###############################################################################
-#<RosettaStone>
-0.0.0.0 16.217.0.0 
-0.0.0.0 rosettastone.com 
-0.0.0.0 launch.rosettastone.com 
-0.0.0.0 amp.rosettastone.com 
-0.0.0.0 resources.rosettastone.com 
-0.0.0.0 updates.rosettastone.com 
-#</RosettaStone>
+#<Local>
+127.0.1.1	MeuPC.localdomain	MeuPC
+#</Local>
 ###############################################################################" >> /etc/hosts
