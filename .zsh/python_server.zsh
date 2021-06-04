1848df4772c45d6d73c4aa19e54cb4a7c9de5480
Julio Batista Silva <julio@juliobs.com>
2021-06-04 13:23:59 -0300

    Python Server using Py3

--- .zsh/python_server.zsh
+++ .zsh/python_server.zsh
@@ -1,7 +1,20 @@
-# Python server
-function python_server()
+# Python 2
+function py2_server()
 {
     local port="${1:-8000}"
-    open "http://localhost:${port}/"
-    python -m SimpleHTTPServer "$port"
+    if (( $+commands[xdg-open] )); then
+        xdg-open "http://localhost:${port}/"
+    fi
+    python2 -m SimpleHTTPServer "$port"
 }
+
+# Python 3
+function py_server()
+{
+    local port="${1:-8000}"
+    if (( $+commands[xdg-open] )); then
+        xdg-open "http://localhost:${port}/"
+    fi
+    python3 -m http.server "$port"
+}
+
