0a6d6b547ac7331e792eefc828c632ce419e34ff
Julio Batista Silva <julio@juliobs.com>
2021-12-27 12:14:54 +0100

    Update keyboards.sh: run for all ids

--- bin/keyboards.sh
+++ bin/keyboards.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 # Notebook keyboard
 keyboard_id=$(
@@ -16,17 +16,18 @@ if [ ! -z "$keyboard_id" ]; then
 fi
 
 # Keychron K2
-keyboard_id=$(
+# See https://zsh.sourceforge.io/Doc/Release/Expansion.html#Parameter-Expansion-Flags
+keyboard_ids=(${(@f)$(
     xinput -list |
     grep "Keychron K2.*keyboard" |
-    sed -e 's/^.*id=\([0-9]\+\).*/\1/' |
-    head -1
-)
-if [ ! -z "$keyboard_id" ]; then
+    sed -e 's/^.*id=\([0-9]\+\).*/\1/'
+)})
+for keyboard_id in $keyboard_ids
+do
     setxkbmap -device $keyboard_id    \
               -layout us              \
               -variant intl           \
               -option lv3:ralt_switch \
               -option compose:rctrl   \
               -option caps:swapescape
-fi
+done
