e791e34f4dcb855776db4a8baeb9fe13f69dbf9f
Julio Batista Silva <julio@juliobs.com>
2022-12-16 19:00:22 +0100

    myip alias

--- .zsh/my_ip.zsh
+++ .zsh/my_ip.zsh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
 if (( $+commands[lynx] )); then
     WWWDUMP='lynx -dump -nolist -width=300 -accept_all_cookies -display_charset=UTF-8'
-    alias meuip="curl -s -m 8 eth0.me || curl -s -m 5 ifconfig.me || curl -s -m 5 icanhazip.com || $WWWDUMP http://www.getip.com | sed -n 's/^Current IP: //p'"
+    alias myip="curl -s -m 8 https://ipinfo.io/ip || curl -s -m 8 eth0.me || curl -s -m 5 ifconfig.me || curl -s -m 5 icanhazip.com || $WWWDUMP http://www.getip.com | sed -n 's/^Current IP: //p'"
 else
-    alias meuip="curl -s -m 8 eth0.me || curl -s -m 5 ifconfig.me || curl -s -m 5 icanhazip.com"
+    alias myip="curl -s -m 8 https://ipinfo.io/ip || curl -s -m 8 eth0.me || curl -s -m 5 ifconfig.me || curl -s -m 5 icanhazip.com"
 fi
 
