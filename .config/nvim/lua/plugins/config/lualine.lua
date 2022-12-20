3b83547b2b14ded435e674339ee0631604cf8da4
Julio Batista Silva <julio@juliobs.com>
2022-12-20 14:39:24 +0100

    Lualine config with git_blame

--- .config/nvim/lua/plugins/config/lualine.lua
+++ .config/nvim/lua/plugins/config/lualine.lua
@@ -1,7 +1,15 @@
 local M = {}
 
 function M.config()
+  vim.g.gitblame_display_virtual_text = 0
+  local git_blame = require('gitblame')
+
   require('lualine').setup {
+    sections = {
+      lualine_c = {
+        { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available }
+      }
+    },
     options = {
       theme = 'tokyonight',
     },
