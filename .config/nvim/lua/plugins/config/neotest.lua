d90cd10ca9379724a1454bd456bebdd8ff5ae4c7
Julio Batista Silva <julio@juliobs.com>
2022-12-30 23:40:34 +0100

    Reorder neotest

--- .config/nvim/lua/plugins/config/neotest.lua
+++ .config/nvim/lua/plugins/config/neotest.lua
@@ -41,11 +41,11 @@ end
 function M.config()
   require('neotest').setup({
     adapters = {
-      require 'neotest-plenary',
-      require 'neotest-rust'
       require('neotest-python')({
         dap = { justMyCode = false },
       }),
+      require('neotest-plenary'),
+      require('neotest-rust'),
       require('neotest-vim-test')({
         ignore_file_types = { 'python', 'vim', 'lua' },
       }),
