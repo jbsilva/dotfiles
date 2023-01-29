662b44b35958fa25282712b3e21381290cde1ab7
Julio Batista Silva <julio@juliobs.com>
2023-01-29 11:44:00 +0100

    VSCode nvim options

--- .config/nvim/lua/vscode/options.lua
+++ .config/nvim/lua/vscode/options.lua
@@ -1,1 +1,16 @@
-require('settings')
+local g = vim.g
+local o = vim.o
+
+-------------------------------------------------------------------------------
+--> Leader key
+-------------------------------------------------------------------------------
+g.mapleader = ' '
+g.maplocalleader = ' '
+
+------------------------------------------------------------------------------
+--> Searching
+-- Ignore case in search patterns except if search pattern contains upper case
+------------------------------------------------------------------------------
+o.ignorecase = true
+o.smartcase = true
+
