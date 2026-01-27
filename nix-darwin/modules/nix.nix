9c1b7ae442aab2382b3707ad068c264fde23c5e3
Julio Batista Silva <julio@juliobs.com>
2026-01-27 10:10:51 +0100

    Nix: Adds default substituters and keys
    Adds default substituters and trusted public keys for
    nixpkgs, nix-community, and nixpkgs-terraform cachix
    caches to improve out-of-the-box experience.

--- nix-darwin/modules/nix.nix
+++ nix-darwin/modules/nix.nix
@@ -4,6 +4,16 @@
     settings = {
       experimental-features = "nix-command flakes";
       extra-nix-path = "nixpkgs=flake:nixpkgs";
+      substituters = [
+        "https://cache.nixos.org"
+        "https://nix-community.cachix.org"
+        "https://nixpkgs-terraform.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixpkgs-terraform.cachix.org-1:8Sit092rIdAVENA3ZVeH9hzSiqI/jng6JiCrQ1Dmusw="
+      ];
     };
 
     nixPath = [
