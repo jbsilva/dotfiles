a99db3566a0996fc48c1eb9e25e3af6c1d28c502
Julio Batista Silva <julio@juliobs.com>
2026-03-23 22:36:53 +0100

    nixpkgs.nix: fix for direnv

--- nix-darwin/modules/nixpkgs.nix
+++ nix-darwin/modules/nixpkgs.nix
@@ -7,5 +7,16 @@
     };
 
     hostPlatform = "aarch64-darwin";
+
+    overlays = [
+      (final: prev: {
+        # direnv 2.37.1 uses -linkmode=external but CGO is disabled on darwin.
+        direnv = prev.direnv.overrideAttrs (old: {
+          env = (old.env or { }) // {
+            CGO_ENABLED = "1";
+          };
+        });
+      })
+    ];
   };
 }
